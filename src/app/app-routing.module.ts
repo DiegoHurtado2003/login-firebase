a1ef0d20ea1992aca5de8f7e3573e87841abb0dd
DiegoHurtado2003 <93648633+DiegoHurtado2003@users.noreply.github.com>
2023-02-21 13:57:12 +0100

    d

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LoginComponent } from './components/login/login.component';
+import { RecoverPasswordComponent } from './components/recover-password/recover-password.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "login", pathMatch: "full"},//Si no pones nada te manda a login
+  { path: "login", component: LoginComponent},
+  { path: "recoverPassword", component: RecoverPasswordComponent},
+  { path: "dashboard", component: DashboardComponent},
+  { path: "**", redirectTo: "login", pathMatch: "full"}//Si pones cualquier cosa(Que no esté arriba) te manda a login
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Modules
 import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AngularFireModule } from '@angular/fire/compat';
+
+// Components
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { RecoverPasswordComponent } from './components/recover-password/recover-password.component';
+import { SpinnerComponent } from './shared/spinner/spinner.component';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    RecoverPasswordComponent,
+    SpinnerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
